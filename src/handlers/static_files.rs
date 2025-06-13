f9e3126dfbc9fe4277b8547237937bbf44e00125
Michael Burch <me@michaelburch.net>
2025-06-13 22:27:59 +0000

    update default styles

--- src/handlers/static_files.rs
+++ src/handlers/static_files.rs
@@ -166,12 +166,16 @@ fn generate_provider_buttons(settings: &VouchrsSettings) -> String {
         // Generate provider class
         let provider_class = format!("provider-{}", provider.name.to_lowercase());
 
-        // Create button without inline style - CSS handles the colors
+        // Get provider icon
+        let provider_icon = get_provider_icon(&provider.name.to_lowercase());
+
+        // Create button with icon
         buttons.push(format!(
             r#"<a href="/auth/sign_in?provider={}" class="provider-button {}">
+                    {}
                     <span>Continue with {}</span>
                 </a>"#,
-            provider.name, provider_class, display_name
+            provider.name, provider_class, provider_icon, display_name
         ));
     }
 
@@ -179,7 +183,8 @@ fn generate_provider_buttons(settings: &VouchrsSettings) -> String {
     if settings.passkeys.enabled {
         buttons.push(
             r#"<button id="passkey-signin" class="provider-button provider-passkey">
-                    <span>🔑 Continue with Passkey</span>
+                    <span class="provider-icon passkey-icon">🔑</span>
+                    <span>Continue with Passkey</span>
                 </button>"#
                 .to_string(),
         );
@@ -218,4 +223,24 @@ const fn get_html_template() -> &'static str {
     {{passkey_script}}
 </body>
 </html>"#
+}
+
+/// Get SVG icon for a specific OAuth provider
+fn get_provider_icon(provider_name: &str) -> &'static str {
+    match provider_name {
+        "googl" => "<svg class=\"provider-icon\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 48 48\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" style=\"display: block;\">
+                        <path fill=\"#EA4335\" d=\"M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z\"></path>
+                        <path fill=\"#4285F4\" d=\"M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z\"></path>
+                        <path fill=\"#FBBC05\" d=\"M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z\"></path>
+                        <path fill=\"#34A853\" d=\"M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z\"></path>
+                        <path fill=\"none\" d=\"M0 0h48v48H0z\"></path>
+                    </svg>",
+        "apple" => "<svg class=\"provider-icon\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" fill=\"white\">
+                        <path d=\"M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09l.01-.01zM12.03 7.25c-.15-2.23 1.66-4.07 3.74-4.25.29 2.58-2.34 4.5-3.74 4.25z\"/>
+                    </svg>",
+        // Default generic icon for all other providers
+        _ => "<svg class=\"provider-icon\" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"white\">
+                        <path d=\"M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z\"/>
+                    </svg>",
+    }
 }
